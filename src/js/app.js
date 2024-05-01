f48321339e9600ab63de7495222a08cced985435
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-05-01 15:17:34 +0200

    Connected firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
+import firebaseConfig from "./firebaseConfig";
+import {initializeApp} from 'firebase/app'
+import {getFirestore, collection, deleteDoc, doc, onSnapshot} from 'firebase/firestore'
+initializeApp(firebaseConfig)
 
-alert("hei")
+const database = getFirestore()
+
+const wineCollection = collection(database, 'wineCollection')
+
+onSnapshot(wineCollection, (snapshot)=>{
+	const wineArray = []
+	snapshot.docs.forEach(item => {
+		wineArray.push({id: item.id, ...item.data()})
+		console.log(wineArray);
+	})
+})
