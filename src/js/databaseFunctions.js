69ca89470be8eafab91f6c004355dc989b6dfc06
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-05-11 11:56:30 +0200

    fixed an issue with average score

--- src/js/databaseFunctions.js
+++ src/js/databaseFunctions.js
@@ -43,6 +43,10 @@ const addWinePage = document.querySelector(".add_wine-page")
 
 const averageScoreContainer = document.querySelector(".tasting-questions_average-score_number")
 
+const addWineNumber = document.querySelector(".tasting-questions-title")
+let wineNumber = 1
+
+
 // getting and updating the average score 
 // FIX THIS
 
@@ -53,15 +57,14 @@ allScore.forEach((score, index)=> {
 		totalScore += Number(score.value) 
 		averageScore = totalScore / (index + 1)
 		averageScore = Math.round(averageScore * 10) / 10
-		console.log(averageScore);
 		averageScoreContainer.textContent = `${averageScore}p`
+		console.log(averageScore);
+		console.log(totalScore);
 	})
 
 })
 
-
-addWineForm.addEventListener("submit", (e)=> {
-	e.preventDefault()
+const addWineToDatabase = ()=> {
 	const newWine = {
 		date: dateInput.value,
 		host: hostInput.value,
@@ -90,16 +93,77 @@ addWineForm.addEventListener("submit", (e)=> {
 	})
 
 	window.scrollTo({top: 0, behavior: "smooth"})
-})
+	totalScore = 0
+	averageScore = 0
+
+}
 
 
-submitButton.addEventListener("click", ()=> {
-	console.log("hei");
+submitButton.addEventListener("click", (e)=> {
+	e.preventDefault()
+	addWineToDatabase()
 	landingPage.classList.add("active-flex")
 	addWinePage.classList.remove("active-flex")
 })	
 
+addAnotherWineButton.addEventListener("click", (e)=> {
+	e.preventDefault()
+	addWineToDatabase()
+	addWineNumber.textContent = `Vin nr. ${wineNumber += 1}`
+})
 
 
+
+
+// submitButton.addEventListener("click", ()=> {
+	
+// 	importArray.forEach((item)=> {
+
+// 		// replacing , with . 
+
+// 		const newPrice = item.Price.replace(",", ".")
+// 		const newMJ = item.MJ.replace(",", ".")
+// 		const newHS = item.HS.replace(",", ".")
+// 		const newBAL = item.BAL.replace(",", ".")
+// 		const newLPW = item.LPW.replace(",", ".")
+// 		const newNIB = item.NIB.replace(",", ".")
+// 		const newJR = item.JR.replace(",", ".")
+// 		const newScore = item.Score.replace(",", ".")
+
+// 		const newWine = {
+// 		date: item.Tasting,
+// 		host: item.Host.toUpperCase(),
+// 		name: item.Wine,
+// 		type: item.Type,
+// 		year: Number(item.Year),
+// 		producer: item.Producer,
+// 		country: item.Country,
+// 		price: Number(newPrice),
+// 		MJ: (newMJ === "") ? 0 : Number(newMJ),
+// 		HS: (newHS === "") ? 0 : Number(newHS),
+// 		BAL: (newBAL === "") ? 0 : Number(newBAL),
+// 		LPW: (newLPW === "") ? 0 : Number(newLPW),
+// 		NIB: (newNIB === "") ? 0 : Number(newNIB),
+// 		JR: (newJR === "") ? 0 : Number(newJR),
+// 		score: Number(newScore)
+		
+// 	}
+
+// 	addDoc(wineCollection, newWine)
+// 	.then(()=> {
+// 		console.log("it worked");
+// 		addWineForm.reset()
+// 		averageScoreContainer.textContent = "- - -"
+// 	})
+// 	.catch((error)=> {
+// 		console.log(error.message);
+// 	})
+// 	})
+	
+
+// 	window.scrollTo({top: 0, behavior: "smooth"})
+// })
+
+// fix this, names have commas in them
 
 export default wineArray
