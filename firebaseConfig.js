129e280e43834970414619ec7ebf5ee70ea6d2da
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-05-13 13:42:40 +0200

    fic auth

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDp0Cn0tDPK3iV0EgYu7jmodqXls0J7s_s',
+  authDomain: 'drum-749ff.firebaseapp.com',
+  projectId: 'drum-749ff',
+  storageBucket: 'drum-749ff.appspot.com',
+  messagingSenderId: '51511662015',
+  appId: '1:51511662015:web:ec92ae63045b948ffdbd0a',
+};
+
+export default firebaseConfig
